bb5f53ba81351dca3f628fb6df7e3edc54fdd343
Ankush Menat <ankush@frappe.io>
2023-08-13 19:33:51 +0530

    fix: allow dict access and unpacks

--- frappe/utils/safe_exec.py
+++ frappe/utils/safe_exec.py
@@ -93,21 +93,13 @@ def safe_exec(script, _globals=None, _locals=None, restrict_commit_rollback=Fals
 def safe_eval(code, eval_globals=None, eval_locals=None):
 	import unicodedata
 
-	whitelisted_globals = {
-		"int": int,
-		"float": float,
-		"long": int,
-		"round": round,
-		# RestrictedPython specific overrides
-		"_getattr_": _get_attr_for_eval,
-	}
 	code = unicodedata.normalize("NFKC", code)
 
 	if not eval_globals:
 		eval_globals = {}
 
 	eval_globals["__builtins__"] = {}
-	eval_globals.update(whitelisted_globals)
+	eval_globals.update(WHITELISTED_SAFE_EVAL_GLOBALS)
 
 	return eval(
 		compile_restricted(code, filename="<safe_eval>", policy=FrappeTransformer, mode="eval"),
@@ -588,3 +580,16 @@ VALID_UTILS = (
 	"get_user_info_for_avatar",
 	"get_abbr",
 )
+
+
+WHITELISTED_SAFE_EVAL_GLOBALS = {
+	"int": int,
+	"float": float,
+	"long": int,
+	"round": round,
+	# RestrictedPython specific overrides
+	"_getattr_": _get_attr_for_eval,
+	"_getitem_": _getitem,
+	"_getiter_": iter,
+	"_iter_unpack_sequence_": RestrictedPython.Guards.guarded_iter_unpack_sequence,
+}
